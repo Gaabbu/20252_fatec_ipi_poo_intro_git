21bde102b92a0ae9fd855282ead727763c973ac6
Gabriel S <gabrielsonada@outlook.com.br>
2025-08-14 17:29:22 -0300

    test(ap01-git): testa subtracao e multiplicacao

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -1,2 +1,9 @@
 import calculadora
-print (calculadora.somar(1, 2))
+a = 2
+b = 2
+
+print (f'{a} + {b} = {calculadora.somar(a, b)}')
+
+print (f'{a} - {b} = {calculadora.subtrair(a, b)}')
+
+print (f'{a} x {b} = {calculadora.multiplicar(a, b)}')
